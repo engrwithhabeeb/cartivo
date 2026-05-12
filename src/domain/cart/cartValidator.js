afd39e9c8b2bc62b96f6fa088f40bd3ff215630e
engrwithhabeeb <tipdevs@gmail.com>
2026-05-12 14:17:53 +0100

    feat(domain/cart): expand validation utility with quantity and existence checks - Add validateQuantityLimit and validateItemAlreadyInCartByIndex to the existing cartValidator.js - To enforce specific business boundaries (1-50 units) and prevent duplicate entries during the addition process - Strengthens the domain's "fail-fast" capabalities, ensuring mutations only proceed when state integrity is guaranteed.

--- src/domain/cart/cartValidator.js
+++ src/domain/cart/cartValidator.js
@@ -5,3 +5,14 @@ export function validateCartIsArray(cart) {
 export function validateItemIsRealObject(item) {
   if (item && Array.isArray(item)) throw new Error("Invalid item type");
 }
+
+export function validateQuantityLimit(quantity) {
+  if (quantity < 1) throw new Error("Quantity must be at least 1.");
+
+  if (quantity > 50) throw new Error("Max quantity of 50 reached.");
+}
+
+export function validateItemAlreadyInCartByIndex(itemIndex, cart) {
+  if (itemIndex !== -1)
+    throw new Error(`${cart[itemIndex].name} already in cart.`);
+}
